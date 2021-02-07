944aac23b72021e7a42ffc4521a66b3470368f00
Lin Gao <aoingl@gmail.com>
2021-02-07 16:14:39 +0800

    Final test on python ci

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from os import path
 import setuptools
 
 pwd = path.abspath(path.dirname(__file__))
-readmefile = path.join(pwd, 'README.md')
-with open(readmefile, encoding='utf-8') as f:
+rdm_file = path.join(pwd, 'README.md')
+with open(rdm_file, encoding='utf-8') as f:
     readme = f.read()
 
 setuptools.setup(
     name='vertx-eventbus-client',
-    version='1.0.1',
+    version='1.0.2',
     packages=['Vertx'],
     author='vertx-dev',
     author_email='vertx-dev@googlegroups.com',
@@ -18,4 +18,3 @@ setuptools.setup(
     long_description=readme,
     long_description_content_type='text/markdown'
 )
-
