f43caf67b666ebab45088910ea9d1bd7199b89ae
Lin Gao <aoingl@gmail.com>
2021-02-08 13:04:21 +0800

    Fix go path

--- Go/build.sh
+++ Go/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cwd=$(cd `dirname $0` && pwd)
-wd="$GOPATH/src/github.com/vert-x3/vertx-eventbus-bridge-clients/Go"
+wd="/go/src/github.com/vert-x3/vertx-eventbus-bridge-clients/Go"
 mkdir -p $wd
 cd $wd
 [ ! -s eventbus ] && ln -s $cwd/eventbus
