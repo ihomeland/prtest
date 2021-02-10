bc40d42e077ff348e47ee15513b01eae6db8d4d6
Lin Gao <aoingl@gmail.com>
2021-02-10 22:25:01 +0800

    debug

--- C/build.sh
+++ C/build.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Linux64
-docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-linux-gnu -it multiarch/crossbuild make
+docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-linux-gnu -i multiarch/crossbuild make
 # Win32
-docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=i686-w64-mingw32 -it multiarch/crossbuild make
+docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=i686-w64-mingw32 -i multiarch/crossbuild make
 # Win64
-docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-w64-mingw32 -it multiarch/crossbuild make
+docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-w64-mingw32 -i multiarch/crossbuild make
 # OSX
-docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-apple-darwin -it multiarch/crossbuild make
+docker run --rm -v $(pwd):/workdir:Z -e CROSS_TRIPLE=x86_64-apple-darwin -i multiarch/crossbuild make
